a33ce46ed5a7e57c785bae0025d9453b3fb558fb
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-02-19 12:36:50 +0300

    yeni dosyalar geldi

--- gitLearn.py
+++ gitLearn.py
@@ -3,4 +3,5 @@ gitAdd = '(git add .) yapıldığında tüm dosyaları add yapıyoruz.'
 gitLog = 'git log ile termina üzerinde değişiklikleri görebiliyoruz.'
 gitIgnore = '.gitignore adlı bir dosya açıp içerisinde ilgili dosyayı yazabiliyoruz. örn gizli.txt veya .txt yazarak ilgili uzantıya sahip tüm dosyaları engeller.'
 gitBr = 'git branch yazarak mevcut branchleri görüntülüyoruz. git branch "yeni" bu komut yeni isimli bir branch oluşturur. (git switch yeni) dersem de o branche geçerim'
-
+kendimeNot = "master branchindekiler yeni açılan branchlere geçerler. Yeni branchler ile birleştirmek için git switch master diyerek ana branche geçeriz"
+Not2 = "Eğer farklı bir branchte yeni bir dosya kod yazarsam tekrar ana branche geçtiğimde o dosya veya kodları göremem. Tekrardan ilgili branche geçersem görürüm"
