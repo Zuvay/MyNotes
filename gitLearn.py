3ad6c436b0355815010bb2e312b40440a597ebf7
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-02-19 12:27:48 +0300

    bu commit yeni branchine geçecek

--- gitLearn.py
+++ gitLearn.py
@@ -2,3 +2,5 @@ git = 'git commit -m "enter your message"'
 gitAdd = '(git add .) yapıldığında tüm dosyaları add yapıyoruz.'
 gitLog = 'git log ile termina üzerinde değişiklikleri görebiliyoruz.'
 gitIgnore = '.gitignore adlı bir dosya açıp içerisinde ilgili dosyayı yazabiliyoruz. örn gizli.txt veya .txt yazarak ilgili uzantıya sahip tüm dosyaları engeller.'
+gitBr = 'git branch yazarak mevcut branchleri görüntülüyoruz. git branch "yeni" bu komut yeni isimli bir branch oluşturur. (git switch yeni) dersem de o branche geçerim'
+
