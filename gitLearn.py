ad2f3d92cd110efe182e47613bc1d3cb73e7d0cc
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-02-19 13:29:46 +0300

    son mesajlar

--- gitLearn.py
+++ gitLearn.py
@@ -8,3 +8,10 @@ Not2 = "Eğer farklı bir branchte yeni bir dosya kod yazarsam tekrar ana branch
 fstforward = "Eğer yeni bir branch açsaydım ve sadece aynı dosya üzerinde değişiklik yapıp merge etseydim buna Fast Forward deniyor. Zaten branch açmasam da aynı noktaya getirecekti beni"
 fstforward2 = "Veya aynı klasör içine farklı bir dosya kaydedip merge etseydim yine bir çakışma olmayacak buna da yine Fast Forward diyeceğiz"
 conflict = "Eğer aynı dosya üzerinde bir branchte farklı diğerinde farklı değişiklikler yapsam bunlar çakışacak yani conflict meydana gelecek"
+restore="Commit etmeden başka bir branche geçmek istiyoruz diyelim bu durumda değişiklikler geçtiğimiz branchte de görünecektir. Bunun olmasını istemiyorsak git status ten sonra git restore (ilgili dosya) yazarak o değişikliği geri alırız"
+"""
+restore edip değişiklikleri kaldırmak yerine stash(zulala) yapabiliriz. İlgili değişikliği yaptıktan sonra git stash ile kodu zulaya atarız
+bu zula dosyada görünmez. git stash list yaparsak stashlediğimiz kodları görebiliriz.
+Bu kodları çıkarmak istediğimizde git stash pop yaparak ilgili dosyaya geri dönmesini sağlayabiliriz. 
+"""
+
