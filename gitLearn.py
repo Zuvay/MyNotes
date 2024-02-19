469439d67ed6bcc9d1e3cdeea3272aa8a591811f
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-02-19 12:12:55 +0300

    yeni mesajlar eklendi

---
+++ gitLearn.py
@@ -0,0 +1,4 @@
+git = 'git commit -m "enter your message"'
+gitAdd = '(git add .) yapıldığında tüm dosyaları add yapıyoruz.'
+gitLog = 'git log ile termina üzerinde değişiklikleri görebiliyoruz.'
+gitIgnore = '.gitignore adlı bir dosya açıp içerisinde ilgili dosyayı yazabiliyoruz. örn gizli.txt veya .txt yazarak ilgili uzantıya sahip tüm dosyaları engeller.'
