e9455f5a80e0105a8de1f5346d21682403b820f0
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-02-19 12:54:08 +0300

    son notlar

--- gitLearn.py
+++ gitLearn.py
@@ -5,3 +5,6 @@ gitIgnore = '.gitignore adlı bir dosya açıp içerisinde ilgili dosyayı yazab
 gitBr = 'git branch yazarak mevcut branchleri görüntülüyoruz. git branch "yeni" bu komut yeni isimli bir branch oluşturur. (git switch yeni) dersem de o branche geçerim'
 kendimeNot = "master branchindekiler yeni açılan branchlere geçerler. Yeni branchler ile birleştirmek için git switch master diyerek ana branche geçeriz"
 Not2 = "Eğer farklı bir branchte yeni bir dosya kod yazarsam tekrar ana branche geçtiğimde o dosya veya kodları göremem. Tekrardan ilgili branche geçersem görürüm"
+fstforward = "Eğer yeni bir branch açsaydım ve sadece aynı dosya üzerinde değişiklik yapıp merge etseydim buna Fast Forward deniyor. Zaten branch açmasam da aynı noktaya getirecekti beni"
+fstforward2 = "Veya aynı klasör içine farklı bir dosya kaydedip merge etseydim yine bir çakışma olmayacak buna da yine Fast Forward diyeceğiz"
+conflict = "Eğer aynı dosya üzerinde bir branchte farklı diğerinde farklı değişiklikler yapsam bunlar çakışacak yani conflict meydana gelecek"
