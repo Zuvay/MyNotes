ab0ec2f9e0bcb43f0f383f8be255ec5d9864940f
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-02-19 13:02:58 +0300

    son hal

---
+++ conflict.py
@@ -0,0 +1,2 @@
+br="burada conflict yapcaz şimdi. bad isimli bir branch oluşturdum bunu da oraya kaydediyorum."
+ss="şimdi master branchine geçtim ve haliyle bu dosya aslında gözükmüyor. Bense şimdi bu değişikliği yapıyorum masterda."
--- gitLearn.py
+++ gitLearn.py
@@ -2,3 +2,5 @@ git = 'git commit -m "enter your message"'
 gitAdd = '(git add .) yapıldığında tüm dosyaları add yapıyoruz.'
 gitLog = 'git log ile termina üzerinde değişiklikleri görebiliyoruz.'
 gitIgnore = '.gitignore adlı bir dosya açıp içerisinde ilgili dosyayı yazabiliyoruz. örn gizli.txt veya .txt yazarak ilgili uzantıya sahip tüm dosyaları engeller.'
+
+#Bunlar da sadece master'daki kodlar demekki
