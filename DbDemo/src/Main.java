c24f2bcf2b8541a0d2eee2c4d25f14e313311568
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-03-29 13:25:08 +0300

    Update Main.java

--- DbDemo/src/Main.java
+++ DbDemo/src/Main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+        //Sorguları tek tek denemek için paket olarak kullandım. Metotlar diğer dosyaların içinde
         InsertQuery.insertQuery();
         UpdateQuery.updateQuery();
     }
