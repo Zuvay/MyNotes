cf14f39bec3eb56dfb3c4362f9baa2f98c4d4aa6
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-04-06 16:21:32 +0300

    Update WordQuiz.java

--- SwingDemo/src/WordQuiz.java
+++ SwingDemo/src/WordQuiz.java
@@ -25,18 +25,21 @@ public class WordQuiz extends JFrame{
         String turkish  = selectQuery.getTurkish();
         String english = selectQuery.getEnglish();
 
-
+        //kelimeleri getir
         selectQuery.selectAndShuffleEnglishWords();
         String englishOne = selectQuery.getEnglishOne();
         String englishTwo = selectQuery.getEnglishTwo();
         String englishThree = selectQuery.getEnglishThree();
 
+        //rastgele atama için listeler oluştur
         JButton[] buttons = {button1, button2, button3, button4};
         String[] variables = {english, englishOne, englishTwo, englishThree};
 
+        //değişkenleri karıştır
         List<String> variableList = Arrays.asList(variables);
         Collections.shuffle(variableList);
 
+        //atamayı yap
         questionLabel.setText(turkish);
         for(int i=0;i<buttons.length;i++){
            buttons[i].setText(variables[i]);
