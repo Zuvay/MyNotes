d8c9799d22f001d223d13c8969b042e6ad790b35
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-04-24 20:46:18 +0300

    Update Inputvetarih.java

--- Inputvetarih.java
+++ Inputvetarih.java
@@ -16,7 +16,7 @@ public class Inputvetarih {
         // nextFloat()	Reads a float value from the user
         // nextInt()	Reads a int value from the user
         // nextLine()	Reads a String value from the user
-        // nextLong()	Reads a long value from the user
+        // nextLong()	Reads a long value from the user.
         // nextShort()	Reads a short value from the user
 
         // Tarih ve Saatler
