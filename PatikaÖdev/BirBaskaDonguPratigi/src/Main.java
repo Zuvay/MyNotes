b781baa37cf04493f105fabf125ef6af06774d93
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-05-03 17:42:04 +0300

    Update Main.java

--- PatikaÖdev/BirBaskaDonguPratigi/src/Main.java
+++ PatikaÖdev/BirBaskaDonguPratigi/src/Main.java
@@ -12,12 +12,12 @@ public class Main {
             if (sayi%4==0 && sayi%2==0){
                 toplam+=sayi;
             }
-        }while(sayi>0);
+        }while(sayi%2==0);
         System.out.println("Toplam -> " +toplam);
     }
 }
-/*Java döngüler ile negatif bir değer girilene kadar kullanıcıdan girişleri kabul eden ve
-girilen değerlerden tek sayıları toplayıp ekrana basan programı yazıyoruz.
+/*
 Ödev
 Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve
-girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.*/
+girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
+*/
