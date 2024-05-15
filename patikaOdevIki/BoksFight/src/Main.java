8b6827602af00d8a77a5fbd707ac9bd0db83c834
ÖmerFaruk <69796388+Zuvay@users.noreply.github.com>
2024-05-15 20:02:26 +0300

    Boks Müsabakası Demo

--- patikaOdevIki/BoksFight/src/Main.java
+++ patikaOdevIki/BoksFight/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("MAÇ BAŞLIYOR");
-        Fighter omer = new Fighter("Ömer" , 15 , 100, 90,5);
+        Fighter omer = new Fighter("Ömer", 15 , 100, 90,5);
         Fighter kerem = new Fighter("Kerem" , 20 , 115, 100,25);
         Ring ring = new Ring(omer,kerem , 90 , 100);
         ring.fight();
